dcbb7e21faa6f801ed6ee2596368fae33ebc8f56
Tony Theodoropoulos <theodot@outlook.com>
2015-11-24 13:18:09 -0500

    Initial Commit

---
+++ TriangleMethod/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TriangleMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(21, 214);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(67, 38);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.drawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawButton;
+    }
+}
+
---
+++ TriangleMethod/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TriangleMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            /* Setup your DrawTriangle method and then uncomment
+             * the following line to test if the method draws a triangle 
+             * correctly. Show me when this part is done.
+             */
+            
+            Pen redPen = new Pen(Color.Red);
+            //DrawTriangle(redPen, 10, 10, 200, 40, 50, 70);
+        }
+
+        /* you need to setup all the parameters for this method. Take a look at the 
+         * assignment for details.
+         */
+        public void DrawTriangle()
+        {
+            /* the first of the three draw lines you will need to create is 
+             * shown below. Use it to help you create the next two lines
+             */
+
+            Graphics formGraphics = this.CreateGraphics();
+            //formGraphics.DrawLine(drawPen, x1, y1, x2, y2);
+        }
+    }
+}
